961ec6b2c655b156afe760ec41104ddc055dff6f
Junya Saito <snfrp352@gmail.com>
2014-01-21 21:08:55 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bari::Application.config.secret_key_base = 'fe0c88b8c5a4c4c8a28dc3e953718af4ecd6a6dca3883fcfefeb479d1345e5275afc417b67f9a67ca2ae231a606c667e52b30da176f82b004c2e60481f33be15'
